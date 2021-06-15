c38693b1ae67956396027fb21f8143400fc87ed0
Michal Miszczyszyn <michal@mmiszy.pl>
2021-06-15 18:56:09 +0200

    More unit tests

--- __tests__/uniqueId.test.ts
+++ __tests__/uniqueId.test.ts
@@ -1,5 +1,7 @@
 import { generateRequestId, generateServerId, parseRequestId, parseServerId } from '../src/utils/uniqueId';
 
+import type { RequestId, ServerId } from '../src/utils/uniqueId';
+
 describe('uniqueId', () => {
   it('should generate unique request ids', () => {
     const ids = Array.from({ length: 10000 }).map(() => generateRequestId());
@@ -17,6 +19,11 @@ describe('uniqueId', () => {
       requestReceivedAt: expect.any(Date),
       requestCounter: expect.any(Number),
     });
+
+    expect(parseRequestId('60c8dabfefb893' as RequestId)).toEqual({
+      requestCounter: 15710355,
+      requestReceivedAt: new Date('2021-06-15T16:52:15.000Z'),
+    });
   });
 
   it('should parse server id', () => {
@@ -26,6 +33,13 @@ describe('uniqueId', () => {
       machineId: expect.any(String),
       processId: expect.any(Number),
       serverCounter: expect.any(Number),
+    });
+
+    expect(parseServerId('60c8dad85f3d1184a9efb894' as ServerId)).toEqual({
+      machineId: '5f3d11',
+      processId: 33961,
+      serverCounter: 15710356,
+      serverStartedAt: new Date('2021-06-15T16:52:40.000Z'),
     });
   });
 });
